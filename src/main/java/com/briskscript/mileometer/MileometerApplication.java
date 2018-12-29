ea3d8e86e934761cd4954f62ddb01f6da14b74e3
Michal Kowalski <michalkowalski@localhost.localdomain>
2018-12-29 20:15:37 +0100

    Initial commit

---
+++ src/main/java/com/briskscript/mileometer/MileometerApplication.java
@@ -0,0 +1,14 @@
+package com.briskscript.mileometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MileometerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MileometerApplication.class, args);
+    }
+
+}
+
